873dcbe7d7638ac7d9a61c9d5856dc0fbe7a2f22
diaup20 <148984593+diaup20@users.noreply.github.com>
2026-04-28 23:18:48 +0300

    Update base path in Vite config for deployment
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+        base: '/pricllist/', // هذا السطر مهم جداً لنشر صفحات GitHub
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
